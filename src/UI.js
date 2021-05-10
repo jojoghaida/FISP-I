c20fe3aa9b222241d36221c85dd03ce76040369f
joe <josephghaida@gmail.com>
2021-05-10 16:58:29 -0400

    basic template

--- src/UI.js
+++ src/UI.js
@@ -3,6 +3,14 @@ import Camera from './Camera';
 const Ui = ( ) => {
   return(
     <div id="Ui">
+      <div id="head">
+        <div>
+          Project Name
+        </div>
+        <div id="sectionAnnot">
+          Section
+        </div>
+      </div>
       <Camera/>
       <div id="navi">
         <div>btn</div>
