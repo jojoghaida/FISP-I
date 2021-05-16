949da94e1a705f9cbfc665551dee2bff361adb35
joe <josephghaida@gmail.com>
2021-05-16 08:49:04 -0400

    some shi9t

--- src/BluePrint.js
+++ src/BluePrint.js
@@ -6,14 +6,21 @@ const BluePrint = ( {} ) => {
     width: "100%",
     height: "100%",
     maxWidth: "100%",
-    maxHeight: "calc(100% - var(--safeAreaTop) - 55px - var(--safeAreaTop) - var(--headSize))"
+    maxHeight: "calc(100% - 55px - var(--safeAreaTop) - var(--headSize))"
   };
   // <div id="drawingOverlay"></div>
   return <div id="bluePrint" style={style}>
     <div id="drawingContainer">
       <img src="CHRISTOPHER.jpg"/>
     </div>
-    { (viewportSize) ? null : <div id="metaControl"><div>param value</div><div></div></div> }
+    {
+      ( viewportSize ) ? null :
+        <div id="metaControl">
+          <div><div>param</div><div>value</div></div>
+          <div><div>param</div><div>value</div></div>
+          <div><div>param</div><div>value</div></div>
+        </div>
+    }
     <div id="expBluePrint" onClick={setViewPortSize.bind( this, !viewportSize )}>exp</div>
   </div>;
 }; export default BluePrint;
